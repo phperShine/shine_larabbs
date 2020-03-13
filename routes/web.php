d13828298f1671b3df2ff15073195a93554f5781
phpShine <phperShine@163.com>
2020-03-13 14:48:44 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,6 @@ Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->na
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 //Route::get('/home', 'HomeController@index')->name('home');
