7b99ba7e3f0bd1e4f04b932abfbf31bc464275e4
phpShine <phperShine@163.com>
2020-03-16 14:50:48 +0800

    翻译URL 标示”

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,9 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 //Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
