1a16b5c27962d3c9f6fc785e69b981dbea085ef6
phpShine <phperShine@163.com>
2020-03-18 15:59:15 +0800

    话题详情

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -87,4 +87,9 @@ class TopicsController extends Controller
         return TopicResource::collection($topics);
     }
 
+    public function show(Topic $topic)
+    {
+        return new TopicResource($topic);
+    }
+
 }
