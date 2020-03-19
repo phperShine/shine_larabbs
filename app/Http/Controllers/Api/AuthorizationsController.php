8e23cf7aeab321f34dcf011739ecd2b8a3ab1770
phpShine <phperShine@163.com>
2020-03-19 21:33:19 +0800

    passport

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -12,11 +12,12 @@ use Psr\Http\Message\ServerRequestInterface;
 use League\OAuth2\Server\AuthorizationServer;
 use Zend\Diactoros\Response as Psr7Response;
 use League\OAuth2\Server\Exception\OAuthServerException;
-
+use App\Traits\PassportToken;
 
 class AuthorizationsController extends Controller
 {
 
+    use PassportToken;
 
     public function store(AuthorizationRequest $originRequest, AuthorizationServer $server, ServerRequestInterface $serverRequest)
     {
@@ -115,9 +116,14 @@ class AuthorizationsController extends Controller
                 break;
         }
 
-        $token= auth('api')->login($user);
 
-        return $this->respondWithToken($token)->setStatusCode(201);
+        $result = $this->getBearerTokenByUser($user, '1', false);
+
+        return response()->json($result)->setStatusCode(201);
+
+        //$token= auth('api')->login($user);
+
+       // return $this->respondWithToken($token)->setStatusCode(201);
     }
 
 
